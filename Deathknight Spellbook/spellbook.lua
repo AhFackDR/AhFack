7188ece01a56a7b3a36f6f96172de8cde531bf77
AhFackDR <46567066+AhFackDR@users.noreply.github.com>
2019-01-10 15:36:46 +0100

    Spellbooks required.
    Replace Spellbooks
    
    DARK_ADDON/ROTATIONS/CLASS

---
+++ Deathknight Spellbook/spellbook.lua
@@ -0,0 +1,107 @@
+local addon, dark_addon = ...
+
+dark_addon.rotation.spellbooks.deathknight = {
+ 
+
+    -- Defensives
+    AntiMagicShell = 48707,
+    IceboundFortitude = 48792,
+    BoneShield = 232049,
+    VampiricBlood = 55233,
+    BloodDrinker = 206931,
+    RuneTap = 194679,
+
+
+    -- Utility
+    Asphyxiate = 108194,
+    DarkCommand = 56222,
+    DeathGrip = 49576,
+    DeathsAdvance = 48265,
+    MindFreeze = 47528,
+    PathOfFrost = 3714,
+    RaiseDead = 46584,
+    DarkSuccor = 101568,
+    SuddenDoom = 49530,
+    ControlUndead = 111673,
+    RaiseAlly = 61999,
+    DeathPact = 48743,
+    ChainsOfIce = 45524,
+    Gnaw = 47481,
+    Leap = 47482,
+    CrimsonScourge = 81136,
+    DancingRuneWeapon = 49028,
+    GorefiendsGrasp = 108199,
+    EmpowerRuneWeapon = 47568,
+    PillarOfFrost = 51271,
+    HornOfWinter = 57330,
+    BlindingSleet = 207167,
+    Rime = 59057,
+    KillingMachine = 51128,
+    FrozenPulse = 194909,
+    ColdHeart = 281208,
+    FrostwyrmsFury = 279302,
+    
+
+
+
+
+
+    -- Damage
+    DeathAndDecay = 43265,
+    DeathCoil = 47541,
+    DeathStrike = 49998,
+    FesteringStrike = 85948,
+    Outbreak = 77575,
+    DarkTransformation = 63560,
+    Apocalypse = 275699,
+    ArmyOfTheDead = 42650,
+    VirulentPlague = 191587,
+    FesteringWounds = 194310,
+    ScourgeStrike = 55090,
+    BloodBoil = 50842,
+    BloodPlague = 55078,
+    HeartStrike = 206930,
+    MarrowRend = 195182,
+    BoneStorm = 194844,
+    MarkOfBlood = 206940,
+    DeathsCaress = 195292,
+    Obliterate = 49020,
+    FrostStrike = 49143,
+    HowlingBlast = 49184,
+    RemorselessWinter = 196770,
+    FrostScythe = 207230,
+    GlacialAdvance = 194913,
+    BreathOfSindragosa = 152279,
+    FrostFever = 55095,
+
+
+
+
+    -- Talents
+    IcyTalons = 194879,
+    InfectedClaws = 207272,
+    AllWillServe = 194916,
+    ClawingShadows = 207311,
+    BurstingSores = 207264,
+    EbonFever = 207269,
+    UnholyBlight = 115989,
+    GripOfTheDead = 273952,
+    DeathsReach = 276079,
+    PestilentPustules = 194917,
+    HarbingerOfDoom = 276023,
+    SoulReaper = 130736,
+    SpellEater = 207321,
+    WraithWalk = 212552,
+    DeathPact = 48743,
+    Pestilence = 277234,
+    Defile = 152280,
+    Epidemic = 207317,
+    ArmyOfTheDamned = 276837,
+    UnholyFrenzy = 207289,
+    SummonGargoyle = 49206,
+
+
+}
+
+dark_addon.rotation.talentbooks.deathknight = {
+}
---
+++ Rogue Spellbook/spellbook.lua
@@ -0,0 +1,84 @@
+local addon, dark_addon = ...
+
+dark_addon.rotation.spellbooks.rogue = {
+
+
+RolltheBones = 193316,
+GhostlyStrike = 196937,
+KillingSpree = 51690,
+AdrenalineRush = 13750,
+BladeRush = 271877,
+MarkedforDeath = 137619,
+BetweentheEyes = 199804,
+Dispatch = 2098,
+Opportunity = 195627,
+PistolShot = 185763,
+SinisterStrike = 193315,
+Stealth = 1784,
+Ambush = 8676,
+SecretTechnique = 280719,
+ShadowBlades = 121471,
+ShadowStrike = 231718,
+ShadowDance = 185313,
+SymbolsOfDeath = 212283,
+Nightblade = 195452,
+Kick = 1766,
+Backstab = 53,
+Eviscerate = 196819,
+ShurikenStorm = 197835,
+Gloomblade = 200758,
+ShrikenTornado = 277925,
+ToxicBlade = 245388,
+Exsanguinate = 200806,
+CrimsonTempest = 121411,
+Garrote = 703,
+Mutilate = 1329,
+Rupture = 1943,
+Vendetta = 79140,
+DancingSteel = 272026,
+SliceAndDice = 5171,
+CloakOfShadows = 31224,
+TricksOfTheTrade = 57934,
+FanOfKnives = 51723,
+Vanish = 1856,
+Feint = 1966,
+Envenom = 32645,
+Gouge = 1776,
+KidneyShot = 408,
+Sprint = 2983,
+Evasion = 5277,
+Riposte = 199754,
+Blind = 2094,
+CrimsonVial = 185311,
+Blindside = 111240,
+DeadlyPoison = 2823,
+Subterfuge = 108208,
+CripplingPoison = 3408,
+
+
+----
+
+Subterfuge = 108208,
+ShroudedSuffocation = 278666,
+Vendetta = 79140,
+Exsanguinate = 200806,
+Vanish = 1856,
+Garrote = 703,
+Rupture = 1943,
+ToxicBlade = 245388,
+CrimsonTempest = 121411,
+Mutilate = 1329,
+FanOfKnives = 51723,
+DeadlyPoison = 2823,
+CripplingPoison = 3408,
+PoisonedKnife = 185565,
+Envenom = 32645,
+KidneyShot = 408,
+
+
+
+
+
+
+  
+}
